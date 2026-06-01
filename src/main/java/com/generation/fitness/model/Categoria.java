f217ebfdc19d32135f015296b3be0be081ff41ea
Bruna Mendes <brunadsantos.mendes@gmail.com>
2026-06-01 11:10:00 -0300

    Criação da model categoria

---
+++ src/main/java/com/generation/fitness/model/Categoria.java
@@ -0,0 +1,34 @@
+package com.generation.fitness.model;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String descricao; // Ex: Roupa, Suplemento
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	
+	}
+
+   
