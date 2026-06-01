9fdf5bd5b239da88e02fd8fa53dbd33a102a6cb7
Patrícia Rocha <ptc.patirocha@gmail.com>
2026-06-01 15:51:27 -0300

    Incluindo o findAll() na Controler Usuário

--- src/main/java/com/generation/fitness/controller/UsuarioController.java
+++ src/main/java/com/generation/fitness/controller/UsuarioController.java
@@ -1,30 +1,43 @@
 package com.generation.fitness.controller;
 
+import java.util.List; 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.generation.fitness.model.Usuario;
 import com.generation.fitness.repository.UsuarioRepository;
 
 import jakarta.validation.Valid;
 
+@RestController
+@RequestMapping("/usuarios")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UsuarioController {
 	
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 	
+	@GetMapping
+	public ResponseEntity<List<Usuario>> getAll() {
+		return ResponseEntity.ok(usuarioRepository.findAll());
+	}
+	
 	@GetMapping("/{id}")
 	public ResponseEntity<Usuario> getById(@PathVariable Long id) {
-	    return usuarioRepository.findById(id)
-	            .map(resposta -> ResponseEntity.ok(resposta))
-	            .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+		return usuarioRepository.findById(id)
+				.map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
 	}
 	
 	@PostMapping("/cadastrar")
@@ -39,24 +52,23 @@ public class UsuarioController {
 				.map(resposta -> ResponseEntity.status(HttpStatus.OK)
 						.body(usuarioRepository.save(usuario)))
 				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
-		
 	}
 		
-		@PostMapping("/logar")
-		public ResponseEntity<Usuario> autenticar(@Valid @RequestBody Usuario usuarioLogin) {
-			return usuarioRepository.findByUsuario(usuarioLogin.getUsuario())
-					.filter(usuarioBanco -> usuarioBanco.getSenha().equals(usuarioLogin.getSenha()))
-					.map(resposta -> ResponseEntity.ok(resposta))
-					.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
-		}
-		
-		@DeleteMapping("/{id}")
-		public ResponseEntity<?> delete(@PathVariable Long id) {
-			return usuarioRepository.findById(id)
-					.map(resposta -> {
-						usuarioRepository.deleteById(id);
-						return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-					})
-					.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
-		}
+	@PostMapping("/logar")
+	public ResponseEntity<Usuario> autenticar(@Valid @RequestBody Usuario usuarioLogin) {
+		return usuarioRepository.findByUsuario(usuarioLogin.getUsuario())
+				.filter(usuarioBanco -> usuarioBanco.getSenha().equals(usuarioLogin.getSenha()))
+				.map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
 	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> delete(@PathVariable Long id) {
+		return usuarioRepository.findById(id)
+				.map(resposta -> {
+					usuarioRepository.deleteById(id);
+					return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+				})
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+}
